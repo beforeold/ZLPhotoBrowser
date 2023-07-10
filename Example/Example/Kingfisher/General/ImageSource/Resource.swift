ac042d36f31d9c236259a4bad8ae89090e39216f
Brook Xi <xipingping@dayuwuxian.com>
2023-07-10 17:22:04 +0800

    rename ImageResource

--- Example/Example/Kingfisher/General/ImageSource/Resource.swift
+++ Example/Example/Kingfisher/General/ImageSource/Resource.swift
@@ -50,10 +50,10 @@ extension Resource {
     }
 }
 
-/// ImageResource is a simple combination of `downloadURL` and `cacheKey`.
+/// KFImageResource is a simple combination of `downloadURL` and `cacheKey`.
 /// When passed to image view set methods, Kingfisher will try to download the target
 /// image from the `downloadURL`, and then store it with the `cacheKey` as the key in cache.
-public struct ImageResource: Resource {
+public struct KFImageResource: Resource {
 
     // MARK: - Initializers
 
@@ -79,7 +79,7 @@ public struct ImageResource: Resource {
 
 /// URL conforms to `Resource` in Kingfisher.
 /// The `absoluteString` of this URL is used as `cacheKey`. And the URL itself will be used as `downloadURL`.
-/// If you need customize the url and/or cache key, use `ImageResource` instead.
+/// If you need customize the url and/or cache key, use `KFImageResource` instead.
 extension URL: Resource {
     public var cacheKey: String { return absoluteString }
     public var downloadURL: URL { return self }
